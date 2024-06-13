a956b4cf23a12e616c67d0e6f5e77d716d05a4dc
Brito <gustavo.ponciano2005@outlook.com>
2024-06-12 22:58:35 -0300

    comparações em set

---
+++ src/set/comparações/Program.java
@@ -0,0 +1,22 @@
+package set.comparações;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Program {
+    public static void main(String[] args) {
+        Set<Product> set = new HashSet<>();
+
+        set.add(new Product("teve", 100.0));
+        set.add(new Product("netbuk", 1000.0));
+        set.add(new Product("tablete", 1050.0));
+
+        Product prod = new Product("netbuk", 1000.0);
+        System.out.println(set.contains(prod)); // no primeiro momento ele da false
+        // ja que por nao ter equals e hashcode ele compara os endereços de memoria
+        // assim claramente sendo diferentes, e não o seu conteudo
+        // porem quando adicionado os dois, ele compara adequadamente
+
+
+    }
+}
